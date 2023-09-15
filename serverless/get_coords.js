d75597aa0f81d721de4fb36cc56d46c179de1d20
gitgudRaize <50393032+gitgudRaize@users.noreply.github.com>
2023-09-15 14:53:19 +0700

    Redeploy

--- serverless/get_coords.js
+++ serverless/get_coords.js
@@ -5,7 +5,7 @@ const { WEATHER_API_KEY } = process.env;
 exports.handler = async (event, context) => {
   const params = JSON.parse(event.body);
   const { entryText } = params;
-  const url = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${entryText}`;
+  const url = `htts://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}&q=${entryText}`;
   const encodedUrl = encodeURI(url);
   try {
     const dataStream = await fetch(encodedUrl);
